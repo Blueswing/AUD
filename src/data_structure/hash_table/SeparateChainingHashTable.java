fad825228fb4fb347c520ae2a9d61ff67f704ede
Blueswing <qq429515154@gmail.com>
2017-06-16 23:03:10 +0200

    init hash table

---
+++ src/data_structure/hash_table/SeparateChainingHashTable.java
@@ -0,0 +1,40 @@
+package data_structure.hash_table;
+
+public class SeparateChainingHashTable<T> implements IHashTable<T> {
+
+	@Override
+	public void insert(T value) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void remove(T value) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean contains(T value) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void empty() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
